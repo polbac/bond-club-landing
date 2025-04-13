e40f9e6c1c5816b3681682a560163a4cd5e918d8
polbac <polbac@gmail.com>
2025-04-13 15:59:04 -0300

    .

--- app/components/monetiza/Convertite.tsx
+++ app/components/monetiza/Convertite.tsx
@@ -9,7 +9,10 @@ export function Convertite() {
     <div className="relative cta-button-block mt-20">
       <BackgroundVideo src="/assets/monetiza/vhs.mp4" />
       <div className="center-block relative" style={{ zIndex: 99 }}>
-        <h1 className="font-condensed relative mb-5">
+        <h1
+          className="font-condensed relative mb-5"
+          style={{ color: "yellow" }}
+        >
           Desempolvá esas pelis del cajón
         </h1>
         <Link href="" className="button-white mt-5">
